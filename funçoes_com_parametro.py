2375d0ed3c85f30ca5fb49c99a5b603981a73e17
thiagomares <39134260+thiagomares@users.noreply.github.com>
2021-03-23 16:40:57 -0300

    Funções com parametro

---
+++ funçoes_com_parametro.py
@@ -0,0 +1,30 @@
+'''
+    Funções com parametro 
+
+    - Quando dizemos funçoes com parametro, falamos de funções que tem recebem dados para serem processados da função
+
+    Se pensarmos em funções, nos podemos ter funções que:
+
+        - Não possuem entradas
+        - Não possuem saidas
+        - Possuem entrada mas não possui saidas
+        - Não possui entrada, mas possui saidas
+        - possui entrada e saidas
+'''
+
+# Exemplo de função com parametro
+
+
+def Função(Parametro):
+    print(Parametro)
+
+
+Função('Thiago')
+
+
+class classe():  # Exemplo de classe no Python
+    def OutraFunção(argumentos):
+        print(argumentos)
+
+
+classe.OutraFunção('Thalia')
