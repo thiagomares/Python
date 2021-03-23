7785e896fe3bd9506824751cf8081965fb51c927
thiagomares <39134260+thiagomares@users.noreply.github.com>
2021-03-23 17:35:49 -0300

    Aula Funções com Parâmetros

--- funçoes_com_parametro.py
+++ funçoes_com_parametro.py
@@ -23,8 +23,34 @@ Função('Thiago')
 
 
 class classe():  # Exemplo de classe no Python
-    def OutraFunção(argumentos):
+    def outra_funcao(argumentos):
         print(argumentos)
 
 
-classe.OutraFunção('Thalia')
+classe.outra_funcao('Thalia')
+
+# Refatorando uma funçao
+
+
+def Quadrado():
+    return 7*7
+
+
+print(Quadrado())
+
+valor_user = input(
+    'Digite o valor que deseja descobrir seu valor ao quadrado \n')
+valor_user = float(valor_user)
+print(valor_user)
+print(type(valor_user))
+
+# Função que soma dois valores
+def Pow(valor1, valor2):
+    return valor1 + valor2
+
+
+print(Pow(input('Digite os dois valores que deseja calcular \n'),
+      input('digite o valores que deseja calcular \n')))
+
+
+# Quando utilizamos funções com parametro, nos devemos enviar o mesmo numero de argumentos que o numero de funções tem. Exemplo: Se temos dois parametros na função, devemos dar dois argumentos para que aquela função seja satisfeita
