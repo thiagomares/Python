85e70ea250174f5e05d260b915336c58086c1974
thiagomares <thiago.mares4@gmail.com>
2022-04-04 19:32:58 -0300

    testes

--- Erros/teste.rb
+++ Erros/teste.rb
@@ -2,7 +2,9 @@ require 'sqlite3'
 
 banco_de_dados = SQLite3::Database.open 'Teste.db'
 banco_de_dados.execute "CREATE TABLE IF NOT EXISTS clientes (id INTEGER AUTO INCREMENT PRIMARY KEY, nome TEXT, telefone INTERGER)"
-banco_de_dados.execute "INSERT INTO clientes (nome, telefone) VALUES (?, ?)", 'Thiago', 31989190738
+name = 'Thiago'
+senha_digitado = 31989190738
+banco_de_dados.execute "INSERT INTO clientes (nome, telefone) VALUES (?, ?)", name, senha_digitado
 
 
 for valor in banco_de_dados.execute "SELECT * FROM clientes"
@@ -10,6 +12,10 @@ for valor in banco_de_dados.execute "SELECT * FROM clientes"
     puts id, nome, telefone
 end
 
-puts telefone if telefone == 31989190738
+if nome == name
+    if telefone == senha_digitado
+        puts "Olá, #{name}"
+    end
+end
 
 banco_de_dados.close if banco_de_dados
