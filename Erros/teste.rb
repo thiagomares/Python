804d09b96267a15171d86f1a179a428ef2962cb1
thiagomares <thiago.mares4@gmail.com>
2022-04-05 10:42:08 -0300

    Testes do DB

--- Erros/teste.rb
+++ Erros/teste.rb
@@ -4,17 +4,21 @@ banco_de_dados = SQLite3::Database.open 'Teste.db'
 banco_de_dados.execute "CREATE TABLE IF NOT EXISTS clientes (id INTEGER AUTO INCREMENT PRIMARY KEY, nome TEXT, telefone INTERGER)"
 name = 'Thiago'
 senha_digitado = 31989190738
-banco_de_dados.execute "INSERT INTO clientes (nome, telefone) VALUES (?, ?)", name, senha_digitado
+# banco_de_dados.execute "INSERT INTO clientes (nome, telefone) VALUES (?, ?)", name, senha_digitado
 
-
-for valor in banco_de_dados.execute "SELECT * FROM clientes"
+dados = []
+for valor in banco_de_dados.execute "SELECT * FROM clientes WHERE nome LIKE ?", name
     id, nome, telefone = valor
-    puts id, nome, telefone
+    dicionario = {'Nome' => nome, 'Senha' => telefone, 'codigo' => id}
+    dados.push(dicionario)
 end
-
+for i in dados
+    puts dados[i]
+end
 if nome == name
     if telefone == senha_digitado
         puts "Olá, #{name}"
+        puts id
     end
 end
 
