326d3e14d00d188821a6e271b62101cad018b2b8
thiagomares <thiago.mares4@gmail.com>
2022-07-07 16:31:12 -0300

    Modo de leitura e escrita de arquivos

---
+++ Leitura de Arquivos/modos_de_arquivo.py
@@ -0,0 +1,8 @@
+"""
+    Modos de abertura de arquivos
+    
+    r = Leitura
+    w = Escrita (sobrescreve o arquivo, caso já exista)
+    a = abre para escrita, contudo inserindo valores no final do arquivo
+    x = abre apenas para criar, falha se existir um arquivo existente
+"""
