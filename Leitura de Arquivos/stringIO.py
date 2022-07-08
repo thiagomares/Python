6f46edda146a1a9bc586d916039e85861f44a9e4
thiagomares <thiago.mares4@gmail.com>
2022-07-08 10:02:41 -0300

    Aula de StringIO

---
+++ Leitura de Arquivos/stringIO.py
@@ -0,0 +1,9 @@
+''' 
+    String IO
+    
+    Para ler ou escrever dados em arquivos em arquivos do sistema operacional o software precisa ter permissão:
+    
+    - Leitura
+    - Escrita
+    
+'''
