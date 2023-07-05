30663bf34b53d3e03548ac0fa44f44ed17843f00
thiagomares <39134260+thiagomares@users.noreply.github.com>
2023-07-05 17:41:33 -0300

    estudando json

--- assertions.py
+++ assertions.py
@@ -1,6 +1,16 @@
+# -*- coding: utf-8 -*-
+
 """ 
     assertions - afirmações
     
     
     em python, utilizamos a palavra reservada assert para realizar
 """
+
+#Teste
+
+def Soma(a, b):
+    assert a > 0 and b > 0, 'Numeros precisam ser positivos'
+    return a + b
+
+print(Soma(2, 3))
---
+++ json.py
@@ -0,0 +1,8 @@
+# Brincando com json
+"""
+    json nada mais é do que uma estrutura de dados que é constantemente utilizado no javascript
+    e que se tornou padrão em todas as linguagens de programação e como saidas
+    de APIs WEB
+"""
+import json
+
