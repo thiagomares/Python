a089782d0b762681cd02d49fb12d5cf2cbe5bc65
thiagomares <thiago.mares4@gmail.com>
2023-09-16 20:22:12 -0300

    checkagem de tipos com mpypy

--- Tipos/CheckagemTipos.py
+++ Tipos/CheckagemTipos.py
@@ -1,4 +1,7 @@
 # Checkagem de tipos com mypy
 """
     A extensão mypy serve para verificar todas as instancias do codigo e verifica se existe alguma incoformidade e lança este erro.
+    
+    aula3.py:12: error: Incompatible types in assignment (expression has type "str", variable has type "int")  [assignment]
+    aula3.py:26: error: Argument 1 to "cumprimentar" has incompatible type "int"; expected "str"  [arg-type]
 """
